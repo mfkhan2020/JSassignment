c439b7954545bad39900303869c9c96b4b03bad0
mfkhan2020 <mfkhan2020@yahoo.com>
2024-09-19 22:59:32 +0500

    update

--- Chapter 5/chapter5_assignment.js
+++ Chapter 5/chapter5_assignment.js
@@ -30,3 +30,172 @@ let num9 = Number.parseInt(prompt("Write your number for modulus"));
 let num10 = Number.parseInt(prompt("Write your number for modulus"));
 let num_total910 = (num9 % num10);
 document.write("The result of modulus " + num9 + " and " + num10 + " is " + num_total910);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+let newnum ;
+document.write("Value after variable declaration is : " + newnum);
+document.write("<br>");
+newnum = 2;
+document.write("Initial value : " + newnum);
+document.write("<br>");
+let incnewnum = ++newnum;
+document.write("Value after increment is : " + incnewnum);
+document.write("<br>");
+let addition_num = incnewnum + 7;
+document.write("Value after addition of '7' is : " + addition_num);
+document.write("<br>");
+let decadd_num = --addition_num;
+document.write("Value after decrement is : " + decadd_num);
+document.write("<br>");
+let remnum = decadd_num % 3;
+document.write("Value after divided by '3'. The remainder is : " + remnum);
+document.write("<br>");
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+let ticket_cost = 600;
+let buying_qty = 5;
+document.write("Total cost to buy 5 tickets to a movie is " + (ticket_cost * buying_qty) + " PKR");
+document.write("<br>");
+
+document.write("TABLE OF 7");
+document.write("<br>");
+let new_table = 7;
+let multply_sign = "X";
+let table_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+document.write(7 + " " + multply_sign + " " + table_values[0] + " = " +(new_table * table_values[0]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[1] + " = " +(new_table * table_values[1]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[2] + " = " +(new_table * table_values[2]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[3] + " = " +(new_table * table_values[3]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[4] + " = " +(new_table * table_values[4]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[5] + " = " +(new_table * table_values[5]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[6] + " = " +(new_table * table_values[6]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[7] + " = " +(new_table * table_values[7]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[8] + " = " +(new_table * table_values[8]) + "<br>");
+document.write(7 + " " + multply_sign + " " + table_values[9] + " = " +(new_table * table_values[9]) + "<br>");
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+let celcius_temp = 25;
+let celcius_to_farenhite = (celcius_temp * 9 / 5) + 32;
+document.write(celcius_temp + " is " + celcius_to_farenhite);
+document.write("<br>");
+let farenhite_temp = 65;
+let farenhite_to_celcius = (farenhite_temp - 32) * 5 /9;
+document.write(farenhite_temp + " is " + farenhite_to_celcius);
+
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+let price_item1 = 650;
+let price_item2 = 100;
+let quantity_item1 = 3;
+let quantity_item2 = 7;
+let shipping_charges = 100;
+document.write("<h1>SHOPPING CART</h1>");
+document.write("<br>");
+document.write("Price of item 1 is " + price_item1);
+document.write("<br>");
+document.write("Quantity of item 1 is " + quantity_item1);
+document.write("<br>");
+document.write("Price of item 2 is " + price_item2);
+document.write("<br>");
+document.write("Quantity of item 2 is " + quantity_item2);
+document.write("<br>");
+document.write("Price of item 1 is " + price_item1);
+document.write("<br>");
+document.write("Shipping Charges is " + shipping_charges);
+document.write("<br>");
+document.write("Price of item 1 is " + price_item1);
+document.write("<br>");
+document.write("Total Cost of your order is " + ((price_item1 * quantity_item1) + (price_item2 * quantity_item2) + shipping_charges));
+document.write("<br>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+document.write("<h1>MARKS SHEET</h1>");
+let total_marks = 980;
+let marks_obtained = 804;
+let obtained_percentage = (marks_obtained / total_marks * 100);
+document.write(obtained_percentage + " %");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h1>CURRENCY IN PKR</h1>");
+let usd = 10;
+let riyal = 25;
+let usd_to_pkr = (usd * 104.80);
+let riyal_to_pkr = (riyal * 28);
+let total_riyal_usd = usd_to_pkr + riyal_to_pkr;
+document.write("Total Currency in PKR : " + total_riyal_usd);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+let calc_num = 6;
+let result_calc_num = (6 + 5 * 10 / 2);
+document.write(result_calc_num);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h1>AGE CALCULATOR</h1>");
+document.write("<br>");
+let current_year = 2024;
+let birth_year = 1989;
+let age_calc = current_year - birth_year;
+document.write("Currnt Year : " + current_year);
+document.write("<br>");
+document.write("Birth Year : " + birth_year);
+document.write("<br>");
+document.write("You are " + age_calc + " years old");
+document.write("<br>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h1>THE GEOMETERIZER</h1>");
+document.write("<br>");
+let circle_radius = 20;
+let circum_of_circle = 2 * 3.142 * circle_radius;
+let area_of_circle = 3.142 * circle_radius * circle_radius;
+document.write("Radius of a circle " + circle_radius);
+document.write("<br>");
+document.write("The circumference is " + circum_of_circle);
+document.write("<br>");
+document.write("The are is " + area_of_circle);
+document.write("<br>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h1>The Lifetime Supply Calculator</h1>");
+document.write("<br>");
+let fav_snack = "chocolate chip";
+let current_age = 35;
+let estimated_max_age = 65;
+let snack_perday = 2;
+let avg_age = estimated_max_age - current_age;
+let total_life = 365 * avg_age;
+let total_life_snack = total_life * snack_perday;
+document.write("You will need " + total_life_snack + " to last you until the ripe old age of " + estimated_max_age);
